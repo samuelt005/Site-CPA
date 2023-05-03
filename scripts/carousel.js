a9e8744325d401575d1f67d5dd46c91457c087d5
Samuel Thomas <samuel.thomas1109@outlook.com>
2023-05-02 22:14:08 -0300

    Inicio do Index e Carrossel

---
+++ scripts/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.carousel_container').slick({
+        dots: true,
+        infinite: true,
+        speed: 1800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false
+    });
+  });
