6a63bdcbc52130537f76bbc3ea2908baa881c5fe
eagleyes005 <samuel.thomas1109@outlook.com>
2023-06-04 14:23:48 -0300

    Varias configurações de responsividade

--- scripts/color_modes.js
+++ scripts/color_modes.js
@@ -1,6 +1,10 @@
 const toggleButton = document.querySelector('.dark_mode_button');
+const imgElements = document.getElementsByClassName('images_header');
 const bodyElement = document.body;
 
 toggleButton.addEventListener('click', () => {
+  for (let i = 0; i < imgElements.length; i++) {
+    imgElements[i].classList.toggle('dark_mode_img');
+  }
   bodyElement.classList.toggle('dark_mode');
 });
